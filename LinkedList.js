244842cf868ef800e2516615cafa849352432cd0
DESKTOP-G7OHJ44\amits <amitsalyan@gmail.com>
2019-04-09 23:34:56 -0700

    Commit

---
+++ LinkedList.js
@@ -0,0 +1,24 @@
+function LinkedList(){
+	this.head=null
+	this.tail=null
+}
+
+function Node(value, next, prev){
+	this.value=value
+	this.prev=prev
+	this.next=next
+}
+
+LinkedList.prototype.addToHead = function(value){
+	const newNode = new Node(value, this.head, null)
+	if(this.head)
+		this.head.prev=newNode
+	else
+		this.tail = newNode
+	this.head=newNode;
+}
+
+const list = new LinkedList();
+list.addToHead(100);
+list.addToHead(200);
+console.log(list);
